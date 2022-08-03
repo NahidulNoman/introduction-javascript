ae1c2351517c53670e8161cc3756b401ec73a921
NahidulNoman <nahidulna5@gmail.com>
2022-08-03 10:56:21 +0600

    variable-complete

---
+++ problem.js
@@ -0,0 +1,53 @@
+// ১. জাভাস্ক্রিপ্ট কি জিনিস এইটা কি জানো?
+// ২. জাভাস্ক্রিপ্ট কিভাবে কাজ করে সেটা কি জানো?
+// ৩. ভেরিয়েবল কি জিনিস?
+// ৪. ভেরিয়েবল কিভাবে ডিক্লেয়ার করে
+
+// var age = 21;
+
+// ৫. ভেরিয়েবল এর মান কিভাবে চেইঞ্জ করে বা আপডেট করে।
+
+// age = [2]
+// console.log(age)
+
+// ৬. কি কি ধরণের ভেরিয়েবল হয়। সেগুলা কি কি (হিন্টস: Numeric, String, Boolean)
+
+// var name = 'noma';
+// var roll = 15;
+// var isCodeHard = true;
+// console.log(typeof(isCodeHard))
+
+// ৭. জাভাস্ক্রিপ এ primitive and non primitive data types কি কি ? উদাহরণ হিসেবে বলো।
+
+// ৮. ভেরিয়েবল এর নাম কিভাবে কিভাবে ডিক্লেয়ার করতে হয়। কোন কোন জিনিস নাম এ লেখা যাবে না। অর্থাৎ Variable এর naming convention সম্পর্কে বলো।
+
+// ৯. দুইটা ভেরিয়েবল এর মধ্যে +, -, *, /, % কিভাবে করে?
+
+// var num1 = 30 ;
+// var num2 = 25 ;
+// var sum = num1 % num2 
+// console.log(sum)
+
+// ১০. শর্টহ্যান্ড গুলো জানতে হবে। বিশেষ করে +=, -=, *=, /= জানতে হবে।
+
+// var num1 = 20;
+// var num2 = 34;
+// num1 /=10;
+// console.log(num1) 
+
+// ১১.. ++ এবং -- এর কাজ কি ? এইটা কি জানো।
+
+// var num1 = 20;
+// var num = num1 +1
+// console.log(num)
+
+// ১২ parseInt, parseFloat, toFixed এইগুলা কি করে?
+
+// var num1 = 0.1;
+// var num2 = 0.2;
+// sum = num1 + num2;
+// sum = sum.toFixed(1);
+// sum = parseFloat(sum)
+// console.log(sum);
+
+
