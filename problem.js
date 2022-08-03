1f3eb571685f8e6c1d37e239f1edcb31b3c6c577
NahidulNoman <nahidulna5@gmail.com>
2022-08-03 16:11:39 +0600

    finaly-all-problem-solved

--- problem.js
+++ problem.js
@@ -206,11 +206,11 @@
 // ৩৪. তুমি এতদিন পর্যন্ত যে যে মডেলের মোবাইল ফোন ইউজ করেছো সেগুলার নাম দিয়ে একটা array বানাও। তারপর একটা while লুপ দিয়ে সেই array এর উপাদান গুলা একটা একটা করে আউটপুট হিসেবে দেখাও
 
 // var usePhone = [ 'Alketel-one-tuch', 'Xiaomi-4X', 'Xiaomi-redmi-note', 'Nokia', 'Vivo' ]
-// var i = usePhone;
-// while(i < usePhone.length ){
+// var i = 0;
+// while(i  < usePhone.length ){
 //     var phone = usePhone[i]
-//     console.log(phone)
-//     usePhone++;
+//     console.log(phone);
+//     i++;
 // }
 
 
