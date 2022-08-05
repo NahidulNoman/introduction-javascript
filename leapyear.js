5f9739d6bd426c26dd63a8a62edcca3f77c85472
NahidulNoman <nahidulna5@gmail.com>
2022-08-05 20:49:49 +0600

    script-tag

--- leapyear.js
+++ leapyear.js
@@ -1,13 +1,26 @@
-function isLeapYear(year){
-    let whichyear = year % 4;
-    if( whichyear === 0){
-       return true;
+// function isLeapYear(year){
+//     let whichyear = year % 4;
+//     if( whichyear === 0){
+//        return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// let theYear = 1344;
+// let confirm =isLeapYear(theYear);
+// console.log(confirm);
+
+function leapYear(year){
+    let whichyear = year % 400;
+    if(whichyear === 0){
+        return true;
     }
     else{
         return false;
     }
 }
-
-let theYear = 1344;
-let confirm =isLeapYear(theYear);
-console.log(confirm);
+let theYear = 2020;
+let confirmYear = leapYear(theYear);
+console.log(confirmYear)
