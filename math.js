75a3e0efe7d94d4a2bc60e7eeecb548743fa396e
NahidulNoman <nahidulna5@gmail.com>
2022-08-07 16:47:16 +0600

    swap-science

---
+++ math.js
@@ -0,0 +1,44 @@
+// Math.pow
+
+let result = Math.pow(2,3);
+// console.log(result);
+
+// Math.abs
+
+// let num1= 10;
+// let num2= 40;
+// let fullNum = Math.abs(num1 -num2);
+// console.log(fullNum)
+// if(fullNum > 7){
+//     console.log('code is right!!')
+// }
+// else{
+//     console.log('code is wrong!!')
+// }
+
+// Math.round
+
+var number = 7.55;
+var unique2 = Math.round(Math.random()*10);
+// console.log(unique2);
+
+// Math.ceil
+
+var unique2 = Math.ceil(number)
+// console.log(unique2);
+
+// Math.floor
+
+var unique3 = Math.floor(number)
+// console.log(unique3);
+
+// random
+
+var unique4 = Math.random()*10
+// console.log(unique4);
+// var unique2 = Math.round(Math.random()*10);
+// console.log(unique2);
+for(i= 0; i<20; i++){
+    var unique2 = Math.round(Math.random()*10);
+    console.log(unique2);
+}
---
+++ swap.js
@@ -0,0 +1,17 @@
+// three way to solved this problem---
+// one way
+
+let first = 6;
+let second = 9;
+// let temp = first;
+// first = second;
+// second =temp;
+// console.log(first,second)
+
+// two way
+
+[first, second] = [second, first]
+console.log(first, second)
+
+// three way
+
